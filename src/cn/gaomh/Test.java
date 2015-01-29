96f5582a755461cbe88d283178bdf5757561e26e
MinghuiGao <ham_777@126.com>
2015-01-29 10:56:00 +0800

    list

--- src/cn/gaomh/Test.java
+++ src/cn/gaomh/Test.java
@@ -1,6 +1,8 @@
 package cn.gaomh;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Test {
 
@@ -12,6 +14,22 @@ public class Test {
 		list.add("3");
 		System.out.println(list.remove(0));
 		System.out.println(list.toString());
+		
+		/// List中是否可以有null
+		List<String> list2 = new ArrayList<String>();
+		list2.add("gaomh");
+		list2.add("");
+		list2.add(null);
+		list2.add("aaa");
+		
+		for(String str : list2){
+			System.out.println("---"+ str);
+		}
+		// ##### List中可以有null，所以在迭代List集合时内部也要非空判断。
+		
+		
+		
+		
 	}
 
 }
